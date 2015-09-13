0455868e6592687719d2641caa64b34fc42a26f9
Charlie Green <geek0795@gmail.com>
2015-09-13 11:35:48 -0700

    Minor additions to server

--- server/server.py
+++ server/server.py
@@ -1,8 +1,20 @@
 #!/usr/bin/env python3
 
-class Server:
-    pass
+class User:
+    ONLINE, OFFLINE = 1, 2
+
+    def __init__():
+        name = 'anonymous'
+
+    def get_status():
+        "Return user's current status (currently either ONLINE or OFFLINE)"
+        return OFFLINE
+
+class server:
+    def __init__():
+        # should probs start a thread here?
+        pass
 
 if __name__ != '__main__':
-    # do stuff
-    pass
+    server = server()           # only one server may exist
+

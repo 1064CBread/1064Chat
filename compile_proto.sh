9fa59478df8f0dfa221402e1dfe76f1fcd79dc84
Kenzie Togami <ket1999@gmail.com>
2015-09-21 15:34:32 -0700

    Add some basic protobuf framework

---
+++ compile_proto.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+sourcedir="protobuf"
+importdir="${sourcedir}_imports"
+outputdir="generated_$sourcedir"
+fileselctor="$sourcedir/"'**.pb'
+
+if test -n "$(find "$fileselector" -print -quit 2>/dev/null)"; then
+    echo Found files to compile, running protoc now...
+else
+    echo No files to compile!
+    exit 1
+fi
+
+protoc --proto_path="$importdir" --python_out="$outputdir" "$fileselector"
+

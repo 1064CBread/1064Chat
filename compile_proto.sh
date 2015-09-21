125d5d00bb16a3eba3bdae6d04b28c8adab2e45e
Kenzie Togami <ket1999@gmail.com>
2015-09-21 16:06:04 -0700

    Actually a defined extension

--- compile_proto.sh
+++ compile_proto.sh
@@ -3,7 +3,7 @@
 sourcedir="protobuf"
 importdir="${sourcedir}_imports"
 outputdir="generated_$sourcedir"
-fileselctor="$sourcedir/"'**.pb'
+fileselctor="$sourcedir/"'**.proto'
 
 if test -n "$(find "$fileselector" -print -quit 2>/dev/null)"; then
     echo Found files to compile, running protoc now...

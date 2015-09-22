e48622ac9225d14c039d1cb913e46d31c76c0417
Kenzie Togami <ket1999@gmail.com>
2015-09-21 20:13:59 -0700

    Setup some cool redirection so output may be parsed easily

--- compile_proto.sh
+++ compile_proto.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+# the sleeps below are for IDEs, syserr and sysout flushing may align strangely
+
 sourcedir="protobuf"
 importdir="${sourcedir}_imports"
 outputdir="generated_$sourcedir"
@@ -7,13 +9,14 @@ fileselector=( $sourcedir/**.proto )
 
 
 if test -n "$(find "$fileselector" -print -quit 2>/dev/null)"; then
-    echo Found files to compile, running protoc now...
+    echo Found files to compile, running protoc now... >&2
+    sleep 0.1
 else
-    echo No files to compile!
+    echo No files to compile! >&2
     exit 1
 fi
 
 mkdir -p "$outputdir"
-protoc --proto_path="$importdir" --proto_path="$sourcedir" --python_out="$outputdir" "$fileselector"
-echo "Finished successfully."
-
+protoc --proto_path="$importdir" --proto_path="$sourcedir" --python_out="$outputdir" "$fileselector" 2>&1
+sleep 0.1
+echo "Finished successfully." >&2

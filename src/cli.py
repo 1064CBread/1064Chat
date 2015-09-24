d6067f65a8bc0ba9c79ea565f671d19b543499f3
Kenzie Togami <ket1999@gmail.com>
2015-09-23 17:08:33 -0700

    Woah. Refactor.sh complete.

---
+++ src/cli.py
@@ -0,0 +1,14 @@
+if __name__ == '__main__':
+    import sys
+    sys.path.append('.')
+    av = sys.argv
+    if len(av) < 2:
+        print('<command> <client|server>')
+        sys.exit(1)
+    if av[1] == 'client':
+        import client
+        print('Client NYI')
+        sys.exit(2)
+    elif av[1] == 'server':
+        from server import app
+        app.run()
--- src/server/app.py
+++ src/server/app.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
 from flask import Flask
 from common.constants import *
+from pathlib import Path
 import os
+import sys
+sys.path.append(str(Path(__file__).parent))
 
 app = Flask(PROGRAM_NAME)
 
@@ -13,13 +16,15 @@ app.config.update(dict(
 ))
 app.config.from_envvar(PROGRAM_NAME.upper() + '_SETTINGS', silent=True)
 
+
 @app.route('/')
 def index():
     links = ["/webui/", "/rest/"]
     htmllinks = ['<a href="{0}">{0}</a>'.format(x) for x in links]
     return "Either go to " + " or ".join(htmllinks)
 
-if __name__ == '__main__':
+
+def run():
     from blueprints import webui
     from blueprints.rest import main as restmain
 

80777c79a424149168a521fb12156d7c2a296ae6
Charlie Green <geek0795@gmail.com>
2015-09-24 22:14:18 -0700

    Removed some code I accidentally committed :|

--- src/static/webui.js
+++ src/static/webui.js
@@ -2,26 +2,7 @@
 
 var WebUI = WebUI || {};
 
-WebUI.initialize = function() {
-    $('#channel-list').resizable({
-	handles: 'e, s',
-	minWidth:100,
-  	maxWidth:900,
-  	resize:function(event,ui){
-    	    var x=ui.element.outerWidth();
-    	    var y=ui.element.outerHeight();
-    	    var ele=ui.element;
-     	    var factor = $(this).parent().width()-x;
-      	    var f2 = $(this).parent().width() * .02999;
-      	    console.log(f2);
-      	    $.each(ele.siblings(),function(idx,item){
-        	ele.siblings().eq(idx).css('height',y+'px');
-          	//ele.siblings().eq(idx).css('width',(factor-41)+'px');
-        	ele.siblings().eq(idx).width((factor-f2)+'px');
-      	    });
-	}
-    });
-    
+WebUI.initialize = function() {    
     // TODO
     console.log("initialized");
 };
